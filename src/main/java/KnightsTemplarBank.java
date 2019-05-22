a0b331595bc4ef1891c7169f8e3356662bb804d3
kunwuliu <kunwuqm@gmail.com>
2019-05-22 04:31:39 -0700

    fourth third

--- src/main/java/KnightsTemplarBank.java
+++ src/main/java/KnightsTemplarBank.java
@@ -1,9 +1,4 @@
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
-import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class KnightsTemplarBank {
 
@@ -18,23 +13,37 @@ public class KnightsTemplarBank {
         int[] result ={-1, -1};
         int[] f = new int[amount + 1];
         int[] n = new int[amount + 1];
+
         f[0] = 0;
         n[0] = 1;
-        for (int i = 1; i <= amount; i++) {
-            int cost = Integer.MAX_VALUE;
-            int count = 0;
-            for (int j = 0; j < options.length; j++) {
-                if (i - options[j] >= 0) {
-                    cost = min(f[i-options[j]] + 1, cost);
-                    count += n[i-options[j]];
+        if (amount > 0) {
+            for (int i = 0; i < options.length; i++) {
+                for (int j = options[i]; j <= amount; j++) {
+                    if (j - options[i] >= 0) {
+                        n[j] += n[j - options[i]];
+                    }
                 }
             }
-            f[i] = cost;
-            n[i] = count;
-            System.out.println("f[" + i + "]=" + cost );
-            System.out.println(count);
+//            System.out.println(n[amount]);
+            result[0] = n[amount];
+            for (int i = 1; i <= amount; i++) {
+                int cost = Integer.MAX_VALUE;
+                for (int j = 0; j < options.length; j++) {
+                    if (i - options[j] >= 0) {
+                        cost = min(f[i-options[j]] + 1, cost);
+                    }
+                }
+                f[i] = cost;
+//                System.out.println("f[" + i + "]=" + cost );
+            }
+            result[1] = f[amount];
+
         }
-        result[1] = f[amount];
+        //无法完成组合
+        if (result[0] == 0) {
+            result[0] = -1;
+            result[1] = -1;
+        }
         return result;
     }
 
@@ -43,7 +52,7 @@ public class KnightsTemplarBank {
     }
 
     public static void main(String[] args) {
-        //long a=System.currentTimeMillis();
+//        long a=System.currentTimeMillis();
 
         KnightsTemplarBank bank = new KnightsTemplarBank();
         //int amount = 10000;
@@ -51,7 +60,7 @@ public class KnightsTemplarBank {
         int[] options = {1, 2, 5, 10, 25};
         System.out.println(Arrays.toString(bank.resolve(amount, options)));
 
-        //System.out.println((System.currentTimeMillis()-a)/1000f+ "秒");
+//        System.out.println((System.currentTimeMillis()-a)/1000f+ "秒");
     }
 
 }
